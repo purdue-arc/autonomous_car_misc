e0af31da4399e1ee88e2dce02c8ac0b63efb3434
James Baxter <baxter26@purdue.edu>
2019-04-04 17:43:50 -0400

    using include file; start of transition

--- autonomous_car_arduino/firmware/hardwareControl.cpp
+++ autonomous_car_arduino/firmware/hardwareControl.cpp
@@ -1,19 +1,36 @@
-#include <ros.h>
-#include <autonomous_car_arduino_msgs/ArduinoCommand.h>
-#include <autonomous_car_arduino_msgs/ArduinoReport.h>
-
-#include <Arduino.h>
-#include <avr/interrupt.h>
+#include "HardwareControl.h"
 
 /*
   Connections:
   Motor Feedback  ->  pin 3   PD3   INT1
-  Motor Control   ->  pin 4   PD4
-  Motor Output    ->  pin 5   PD5   PWM
-  Servo Output    ->  pin 6   PD6   PWM
+  Reciever Drive  ->  pin 4   PD4
+  Motor Signal    ->  pin 5   PD5   PWM
+  Servo Signal    ->  pin 6   PD6   PWM
   Servo Feedback  ->  pin 14  PC0   A0
-  Servo Control   ->  pin 13  PB5
+  Reciever Steer  ->  pin 13  PB5
 */
+
+const byte MOTOR_FEEDBACK_PIN = 3;
+const byte RECIEVER_DRIVE_PIN = 4;
+const byte MOTOR_SIGNAL_PIN = 5;
+const byte SERVO_SIGNAL_PIN = 6;
+const byte RECIEVER_STEER_PIN = 13;
+const byte SERVO_FEEDBACK_PIN = 14;
+
+#define BUFFER_SIZE 10
+
+float getBufferAverage(float * buffer [BUFFER_SIZE])
+{
+  float averageEffort = 0;
+  cli();
+  for (byte i = 0; i < BUFFER_SIZE; i++)
+  {
+    averageEffort += *buffer[i];
+  }
+  sei();
+  averageEffort /= BUFFER_SIZE;
+  return averageEffort;
+}
 
 void commandCallback(const autonomous_car_arduino_msgs::ArduinoCommand& msg_command)
 {
@@ -22,7 +39,11 @@ void commandCallback(const autonomous_car_arduino_msgs::ArduinoCommand& msg_comm
 
 void populateReport(autonomous_car_arduino_msgs::ArduinoReport *msg_report)
 {
-  // Do something
+  // Populate Message
+  msg_report->sensor_feedback.steering_angle = analogRead(SERVO_FEEDBACK_PIN);
+  msg_report->sensor_feedback.speed = getBufferAverage(&motor_feedback_buffer);
+  msg_report->radio_command.steering_angle = getBufferAverage(&steer_effort_buffer);
+  msg_report->radio_command.speed = getBufferAverage(&drive_effort_buffer);
   msg_report->drive_battery_voltage = 0.0;
   msg_report->computer_battery_voltage = 0.0;
 }
@@ -47,23 +68,33 @@ unsigned long last_report_ms;
 unsigned long last_spin_ms;
 
 volatile unsigned long last_motor_edge_us;
-volatile float current_motor_effort;
+volatile float motor_feedback_buffer [BUFFER_SIZE] = {};
+volatile byte motor_buffer_index;
 
+// Reading input from RC controller
 volatile unsigned long last_steer_edge_us;
-volatile bool last_steer_state;
-volatile float current_steer_effort;
+volatile float steer_effort_buffer [BUFFER_SIZE] = {};
+volatile byte steer_buffer_index;
 
 volatile unsigned long last_drive_edge_us;
-volatile bool last_drive_state;
-volatile float current_drive_effort;
+volatile float drive_effort_buffer [BUFFER_SIZE] = {};
+volatile byte drive_buffer_index;
 
 void setup()
 {
   last_report_ms = 0;
   last_spin_ms = 0;
+
   nh.initNode();
   nh.advertise(reporter);
   nh.subscribe(commander);
+
+  pinMode(MOTOR_FEEDBACK_PIN, INPUT;
+  pinMode(RECIEVER_DRIVE_PIN, INPUT);
+  pinMode(MOTOR_SIGNAL_PIN, OUTPUT);
+  pinMode(SERVO_SIGNAL_PIN, OUTPUT);
+  pinMode(RECIEVER_STEER_PIN, INPUT);
+  pinMode(SERVO_FEEDBACK_PIN, INPUT)
 
   attachInterrupt(1, updateMotorSpeed, FALLING);
 
@@ -90,36 +121,48 @@ void loop()
   }
 }
 
-// Port B callback, PB5, pin 13, servo control
-void ISR(PCINT0_vect)
+// Port B callback, PB5, pin 13, reciever steer
+ISR(PCINT0_vect)
 {
-  // Get the time
+  // Get the time. Resolution = 8 us
   unsigned long time = micros();
-  if(last_steer_state)
+  if(digitalRead(RECIEVER_STEER_PIN) == LOW)
   {
     // This is the falling edge, calculate effort
-    current_steer_effort = (time - (last_steer_edge_us + 500)) / 500.0;
+    float effort = (time - (last_steer_edge_us + 500)) / 500.0;
+    current_steer_effort[steer_buffer_index] = effort;
+    steer_buffer_index++;
+    if (steer_buffer_index >= BUFFER_SIZE)
+    {
+      steer_buffer_index = 0;
+    }
   }
   else
   {
+    // This is the rising edge, record the time
     last_drive_edge_us = time;
   }
-  last_steer_state = !last_steer_state;
 }
 
-// Port D callback, PD4, pin 4, motor control
-void ISR(PCINT2_vect)
+// Port D callback, PD4, pin 4, reciever drive
+ISR(PCINT2_vect)
 {
-  // Get the time
+  // Get the time. Resolution = 8 us
   unsigned long time = micros();
-  if(last_drive_state)
+  if(digitalRead(RECIEVER_DRIVE_PIN) == LOW)
   {
     // This is the falling edge, calculate effort
-    current_drive_effort = (time - (last_drive_edge_us + 500)) / 500.0;
+    float effort = (time - (last_drive_edge_us + 500)) / 500.0;
+    current_drive_effort[drive_buffer_index] = effort;
+    drive_buffer_index++;
+    if (drive_buffer_index >= BUFFER_SIZE)
+    {
+      drive_buffer_index = 0;
+    }
   }
   else
   {
+    // This is the rising edge, record the time
     last_drive_edge_us = time;
   }
-  last_drive_state = !last_drive_state;
 }
---
+++ autonomous_car_arduino/include/autonomous_car_arduino/hardwareControl.h
@@ -0,0 +1,59 @@
+#include <ros.h>
+#include <autonomous_car_arduino_msgs/ArduinoCommand.h>
+#include <autonomous_car_arduino_msgs/ArduinoReport.h>
+
+#include <Arduino.h>
+#include <Servo.h>  // Uses timer 1
+#include <avr/interrupt.h>
+
+class HardwareControl
+{
+public:
+  // Determine average value based off pointer to buffer
+  float getBufferAverage(float * buffer [BUFFER_SIZE]);
+
+  // ROS callbacks
+  void commandCallback(const autonomous_car_arduino_msgs::ArduinoCommand& msg_command);
+  void populateReport(autonomous_car_arduino_msgs::ArduinoReport *msg_report);
+
+  // Interrupts
+  void updateMotorSpeed();
+  // Port B callback, PB5, pin 13, reciever steer
+  // Port D callback, PD4, pin 4, reciever drive
+
+  // Pin assignments
+  const byte MOTOR_FEEDBACK_PIN;
+  const byte RECIEVER_DRIVE_PIN;
+  const byte MOTOR_SIGNAL_PIN;
+  const byte SERVO_SIGNAL_PIN;
+  const byte RECIEVER_STEER_PIN ;
+  const byte SERVO_FEEDBACK_PIN;
+
+  const byte BUFFER_SIZE;
+  const byte REPORT_DELAY_MS;
+  const byte SPIN_DELAY_MS;
+
+private:
+  // ROS stuff
+  ros::NodeHandle nh;
+  autonomous_car_arduino_msgs::ArduinoReport msg_report;
+  ros::Subscriber<autonomous_car_arduino_msgs::ArduinoCommand> commander;
+  ros::Publisher reporter;
+
+  // Delay between sending status messages
+  unsigned long last_report_ms;
+  unsigned long last_spin_ms;
+
+  volatile unsigned long last_motor_edge_us;
+  volatile float motor_feedback_buffer [];
+  volatile byte motor_buffer_index;
+
+  // Reading input from RC controller
+  volatile unsigned long last_steer_edge_us;
+  volatile float steer_effort_buffer [];
+  volatile byte steer_buffer_index;
+
+  volatile unsigned long last_drive_edge_us;
+  volatile float drive_effort_buffer [];
+  volatile byte drive_buffer_index;
+}
