aa35562bc7760c41eb009f25732428715ba28668
James Baxter <baxter26@purdue.edu>
2019-04-06 20:59:50 -0400

    minor bugs

--- imu_i2c/src/MPU6050.cpp
+++ imu_i2c/src/MPU6050.cpp
@@ -24,15 +24,15 @@ imu_data MPU6050::readIMU() {
 
   imu_data measurement;
 
-  measurement.accelerometer_x_mps = convertRawToEffort(burstData, ACCEL_REGISTER_X2 - IMU_BURST_BYTES) * accelRange * ACCEL_CONSTANT_MPS;
-  measurement.accelerometer_y_mps = convertRawToEffort(burstData, ACCEL_REGISTER_Y2 - IMU_BURST_BYTES) * accelRange * ACCEL_CONSTANT_MPS;
-  measurement.accelerometer_z_mps = convertRawToEffort(burstData, ACCEL_REGISTER_Z2 - IMU_BURST_BYTES) * accelRange * ACCEL_CONSTANT_MPS;
+  measurement.accelerometer_x_mps = convertRawToEffort(burstData, ACCEL_REGISTER_X2 - IMU_BURST_REGISTER) * accelRange * ACCEL_CONSTANT_MPS;
+  measurement.accelerometer_y_mps = convertRawToEffort(burstData, ACCEL_REGISTER_Y2 - IMU_BURST_REGISTER) * accelRange * ACCEL_CONSTANT_MPS;
+  measurement.accelerometer_z_mps = convertRawToEffort(burstData, ACCEL_REGISTER_Z2 - IMU_BURST_REGISTER) * accelRange * ACCEL_CONSTANT_MPS;
 
-  measurement.temperature_deg_c = convertRawToEffort(burstData, TEMP_REGISTER_2 - IMU_BURST_BYTES) / TEMP_CONSTANT_DIV + TEMP_CONSTANT_ADD;
+  measurement.temperature_deg_c = convertRawToEffort(burstData, TEMP_REGISTER_2 - IMU_BURST_REGISTER) / TEMP_CONSTANT_DIV + TEMP_CONSTANT_ADD;
 
-  measurement.gyro_x_radps = convertRawToEffort(burstData, GYRO_REGISTER_X2 - IMU_BURST_BYTES) * gyroRange * GYRO_CONSTANT_RADPS;
-  measurement.gyro_y_radps = convertRawToEffort(burstData, GYRO_REGISTER_Y2 - IMU_BURST_BYTES) * gyroRange * GYRO_CONSTANT_RADPS;
-  measurement.gyro_z_radps = convertRawToEffort(burstData, GYRO_REGISTER_Z2 - IMU_BURST_BYTES) * gyroRange * GYRO_CONSTANT_RADPS;
+  measurement.gyro_x_radps = convertRawToEffort(burstData, GYRO_REGISTER_X2 - IMU_BURST_REGISTER) * gyroRange * GYRO_CONSTANT_RADPS;
+  measurement.gyro_y_radps = convertRawToEffort(burstData, GYRO_REGISTER_Y2 - IMU_BURST_REGISTER) * gyroRange * GYRO_CONSTANT_RADPS;
+  measurement.gyro_z_radps = convertRawToEffort(burstData, GYRO_REGISTER_Z2 - IMU_BURST_REGISTER) * gyroRange * GYRO_CONSTANT_RADPS;
 
   return measurement;
 }
@@ -40,12 +40,5 @@ imu_data MPU6050::readIMU() {
 double convertRawToEffort(uint8_t (&rawData) [], uint8_t startIndex)
 {
   int16_t raw16 = ((int16_t)rawData(startIndex) << 8) | rawData(++startIndex);
-  if (raw16 < 0)
-  {
-    return ((double)raw16) / -INT16_MIN
-  }
-  else
-  {
-    return ((double)raw16) / INT16_MAX
-  }
+  return ((double)raw16) / INT16_MAX
 }
